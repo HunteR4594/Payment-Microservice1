41a943674c1469fd0ff5193066a89efbc4c285ac
Brenan Collado <colladobrenanwilliard@gmail.com>
2025-12-28 02:13:06 +0800

    Add frontend voucher page and update gitignore

---
+++ frontend-voucher-page/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MyVouchers from './MyVouchers';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{ minHeight: '100vh', backgroundColor: '#ffffff' }}>
+      <MyVouchers />
+    </div>
+  );
+}
+
+export default App;
